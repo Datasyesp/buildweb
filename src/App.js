7117c2376e1e4fae3e45a3e3cea222ffe1fc9fcb
Datasyesp <datasyesp@gmail.com>
2025-02-28 00:00:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React, { useEffect } from "react";
+import "./App.css";
+
+export default function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.href = "https://forms.zohopublic.in/contactyesp1/form/YESPTechRequirementGatheringForm/formperma/rAQP8PpHlnw9721tr9V4NxNkuIJvjQOKnYvALP1gqKE";
+    }, 3000);
+  }, []);
+
+  return <div>Redirecting...</div>;
+}
